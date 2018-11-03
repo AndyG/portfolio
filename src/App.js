9d957884463408b43ecbaaf45880467b398558fa
AndyG <anderson.garron@gmail.com>
2018-11-03 15:45:51 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      punctuation: '!!',
+    };
+  }
+
+  handleClick = () => {
+    this.setState({punctuation: '.'});
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload
+            {this.state.punctuation}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
         </header>
+
+        <button onClick={this.handleClick}>Test Button</button>
       </div>
     );
   }
